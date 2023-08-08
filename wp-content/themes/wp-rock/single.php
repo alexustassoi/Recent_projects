bb426056c25f1a35f0a80ae7dad85af676b4e5bd
Ranray1408 <51212159+Ranray1408@users.noreply.github.com>
2023-08-08 18:04:57 +0300

    blog, single post (adaptive)

--- wp-content/themes/wp-rock/single.php
+++ wp-content/themes/wp-rock/single.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * General template for single posts
+ * General template for single posts.
  *
  * @package WP-rock
  * @since 4.4.0
@@ -8,23 +9,96 @@
 
 get_header();
 
-do_action( 'wp_rock_before_page_content' );
+do_action('wp_rock_before_page_content');
+
+global $global_options;
+$fields = get_fields();
+
+$back_text = get_field_value($global_options, 'back_text');
+$share_text = get_field_value($global_options, 'share_text');
+$socials_in_single_post = get_field_value($global_options, 'socials_in_single_post');
+
+$content = get_field_value($fields, 'content');
+$actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 4,
+);
+
+$query = new WP_Query($args);
 ?>
-    <div class="container single-blog">
-        <div class="row">
-            <div class="col-12">
+<section class="single-post">
+    <div class="single-post__container container">
+        <h1 class="single-post__title"><?php echo do_shortcode(get_the_title()); ?></h1>
+        <div class="single-post__wrapper">
+            <div class="single-post__content">
+                <time class="single-post__date"><?php echo get_the_date('d.m.Y'); ?></time>
+                <div class="single-post__inner-text"><?php echo do_shortcode($content); ?></div>
+                <div class="single-post__buttons-wrapper">
+                    <?php
+                    if (!empty($back_text)) {
+                        echo '<button onclick="history.back()" class="single-post__back arrow-link-reverse arrow-link">
+                                    ' . do_shortcode($back_text) . '
+                                </button>';
+                    }
+                    ?>
+                    <div class="single-post__social">
+
+                        <?php
+                        if (!empty($share_text)) {
+                            echo '<span>' . do_shortcode($share_text) . '</span>';
+                        }
+                        ?>
+                        <button>
+                            <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.36369 2.18184C4.36369 3.38684 3.38684 4.36369 2.18184 4.36369C0.976845 4.36369 0 3.38684 0 2.18184C0 0.976845 0.976845 0 2.18184 0C3.38684 0 4.36369 0.976845 4.36369 2.18184ZM0 5.45456H4.36359V17.4546H0V5.45456ZM10.9091 7.39641C10.9091 7.39641 11.3018 5.45456 14.5746 5.45456C17.0618 5.45456 18.5454 7.49456 18.5454 10.9091V17.4546H14.1818V10.9091C14.1818 9.69816 13.8546 8.72728 12.7637 8.72728C10.9091 8.72728 10.9091 10.9091 10.9091 10.9091V17.4546H6.54553V5.45456H10.9091V7.39641Z" fill="#292B2E" />
+                            </svg>
+                            <script type="IN/Share" data-url="https://www.linkedin.com"></script>
+                        </button>
+
+                        <button>
+                            <svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2.18184 4.36369V6.54553H0V9.81825H2.18184V19.6364H6.54544V9.81825H9.44728L9.81816 6.54553H6.54544V4.63641C6.54544 3.75281 6.63272 3.28369 7.99631 3.28369H9.81816V0H6.89456C3.40369 9.375e-05 2.18184 1.63641 2.18184 4.36369Z" fill="#292B2E" />
+                            </svg>
+                            <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="" data-size="">
+                                <a target="_blank"
+                                    href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $actual_link; ?>" class="fb-xfbml-parse-ignore"></a>
+                            </div>
+                        </button>
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="single-post__related-posts">
                 <?php
-                if ( have_posts() ) :
-                    while ( have_posts() ) :
-                        the_post();
-                        the_content();
+                if ($query->have_posts()) {
+                    while ($query->have_posts()) : $query->the_post();
+
+                        $thumb = get_the_post_thumbnail_url() ?
+                            '<a href="' . get_the_permalink() . '" class="single-post__related-post-thumb">
+                                <img src="' . get_the_post_thumbnail_url() . '" alt="thumbnail">
+                            </a>' : '';
+
+                        echo '<div class="single-post__related-post">
+                                    <time class="single-post__related-post-date">' . get_the_date('d.m.Y') . '</time>
+                                    <p class="single-post__related-post-title">' . get_the_title() . '</p>
+                                    ' . $thumb . '
+                                    <a href="#" class="single-post__related-post-link circle-link"></a>
+                                </div>';
+
                     endwhile;
-                endif;
+                }
                 ?>
+
             </div>
         </div>
     </div>
+</section>
 
-<?php do_action( 'wp_rock_after_page_content' ); ?>
 <?php
+
+do_action('wp_rock_after_page_content');
+
 get_footer();
