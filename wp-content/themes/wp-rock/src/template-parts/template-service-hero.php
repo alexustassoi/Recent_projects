a6f7d315bf3612b003ad6823971387ce912161af
Eugene <eugeneweblab@gmail.com>
2023-08-15 15:50:09 +0300

    Fixed services hero block

--- wp-content/themes/wp-rock/src/template-parts/template-service-hero.php
+++ wp-content/themes/wp-rock/src/template-parts/template-service-hero.php
@@ -15,33 +15,34 @@ $post_desc  = (isset($args['post_desc'])) ? $args['post_desc'] : null;
 ?>
 
 <section class="services-hero">
-    <?php
-    if ($hero_img) {
-        echo ' <figure class="services-hero__image">
-                    <img src="' . $hero_img . '" alt="image">
-                </figure>';
-    }
-    ?>
-
     <div class="services-hero__container container">
-        <div class="services-hero__content">
+        <div class="services-hero__content-wrapper">
             <?php
-            echo ($hero_title)
-                ? '<h2 class="services-hero__title">' . do_shortcode($hero_title) . '</h2>'
-                : '';
-
-            echo ($location)
-                ? '<div class="services-hero__location">' . do_shortcode($location) . '</div>'
-                : '';
-
-            echo ($post_desc)
-                ? '<div class="services-hero__post-desc">' . do_shortcode($post_desc) . '</div>'
-                : '';
-
-            echo ($hero_desc)
-                ? '<div class="services-hero__desc">' . do_shortcode($hero_desc) . '</div>'
-                : '';
+            if ($hero_img) {
+                echo ' <figure class="services-hero__image">
+                    <img height="548" width="960" src="' . $hero_img . '" alt="image">
+                </figure>';
+            }
             ?>
+            <div class="services-hero__content">
+                <?php
+                echo ($hero_title)
+                    ? '<h2 class="services-hero__title">' . do_shortcode($hero_title) . '</h2>'
+                    : '';
+
+                echo ($location)
+                    ? '<div class="services-hero__location">' . do_shortcode($location) . '</div>'
+                    : '';
+
+                echo ($post_desc)
+                    ? '<div class="services-hero__post-desc">' . do_shortcode($post_desc) . '</div>'
+                    : '';
+
+                echo ($hero_desc)
+                    ? '<div class="services-hero__desc">' . do_shortcode($hero_desc) . '</div>'
+                    : '';
+                ?>
+            </div>
         </div>
     </div>
 </section>
