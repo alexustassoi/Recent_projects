2b322a94093fe6c087a29d8ae8e1292ba699a49b
Eugene <eugeneweblab@gmail.com>
2023-08-15 10:14:41 +0300

    Fixed blog page

--- wp-content/themes/wp-rock/index.php
+++ wp-content/themes/wp-rock/index.php
@@ -18,28 +18,28 @@ $blog_link_text = get_field_value($global_options, 'blog_link_text');
 $blog_link_text = (!is_null($blog_link_text)) ? $blog_link_text : 'Read more';
 
 ?>
-<div class="blog">
+<div class="blog-page">
     <div class="container">
         <?php
-        echo (!empty($blog_title)) ? '<h1 class="blog__title">' . do_shortcode($blog_title) . '</h1>' : '';
+        echo (!empty($blog_title)) ? '<h1 class="blog-page__title">' . do_shortcode($blog_title) . '</h1>' : '';
         ?>
-        <div class="blog__inner">
+        <div class="blog-page__inner">
             <?php if (have_posts()) : ?>
                 <?php
                 while (have_posts()) :
                     the_post();
 
-                    $excerpt = get_the_excerpt() ? '<p class="post__excerpt">' . get_the_excerpt() . '</p>' : '';
-                    $thumb = get_the_post_thumbnail_url() ? '<a href="' . get_the_permalink() . '" class="post__thumb">
+                    $excerpt = get_the_excerpt() ? '<p class="post-card__excerpt">' . get_the_excerpt() . '</p>' : '';
+                    $thumb = get_the_post_thumbnail_url() ? '<a href="' . get_the_permalink() . '" class="post-card__thumb">
                                                                 <img src="' . get_the_post_thumbnail_url() . '" alt="thumbnail">
                                                             </a>' : '';
 
 
-                    echo '<div class="post">
-                        <time class="post__date">' . get_the_date('d.m.Y') . '</time>
+                    echo '<div class="post-card">
+                        <time class="post-card__date">' . get_the_date('d.m.Y') . '</time>
                         ' . do_shortcode($excerpt) . '
                         ' . $thumb . '
-                        <a class="post__link arrow-link" href="' . get_the_permalink() . '">
+                        <a class="post-card__link arrow-link" href="' . get_the_permalink() . '">
                             ' . $blog_link_text . '
                         </a>
                     </div>';
